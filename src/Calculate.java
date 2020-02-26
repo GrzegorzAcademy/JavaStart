75ecadf1a09efbd2af835b2ee8b122a105c4b9d1
Grzegorz Nagórski <59772089+GrzegorzAcademy@users.noreply.github.com>
2020-02-26 20:25:12 +0100

    task 2 java start

---
+++ src/Calculate.java
@@ -0,0 +1,9 @@
+public class Calculate {
+    public static void main(String[] args) {
+        Calculator add = new Calculator();
+        System.out.println(add.add(2,6));
+        Calculator sub = new Calculator();
+        System.out.println(sub.subtract(6,2));
+
+    }
+}

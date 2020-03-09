39d75193f7375108d378ce4ff8738941687dc854
Grzegorz Nagórski <59772089+GrzegorzAcademy@users.noreply.github.com>
2020-03-09 10:49:13 +0100

    super

---
+++ src/fruits/GloceryShop.java
@@ -0,0 +1,8 @@
+package fruits;
+
+public class GloceryShop {
+    public static void main(String[] args) {
+        Apple apple = new Apple(200, "champion");
+        System.out.println(apple.getInfo());
+    }
+}
